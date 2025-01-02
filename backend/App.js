43e9efd646e007c50db969164447c7f915bdc0c8
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-01-02 12:01:45 +0530

    Server Started

--- backend/App.js
+++ backend/App.js
@@ -1,2 +1,15 @@
 import express from "express";
-import dotenv from "dotenv";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+})
+
+export default app;
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import http from "http";
+import app from "./App.js"
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`Server is running on PORT:${port} `)
+})
