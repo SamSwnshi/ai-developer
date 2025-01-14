9bce675f3e411cf610d38e86c52e4a4e1429e9cb
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-01-14 15:18:31 +0530

    socket io middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import http from "http";
 import app from "./App.js";
 import dotenv from "dotenv";
 import { Server } from "socket.io";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -10,6 +11,28 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 const io = new Server(server);
+
+io.use((socket,next)=>{
+    try {
+        const token = socket.handshake.auth?.token || socket.handshake.headers.authorization?.split(" ")[1];
+        
+        if(!token){
+            return next(new Error("Authentication Error!"))
+        }
+
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+        if(!decoded){
+            return next(new Error("Authentication Error!"))
+        }
+
+        socket.user = decoded;
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
 
 io.on("connection", (socket) => {
   console.log("a new connection");
