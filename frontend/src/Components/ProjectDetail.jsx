8c6154a1f9207263fff27679ee1f64f6fe853056
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-01-17 23:14:59 +0530

    new changes

--- frontend/src/Components/ProjectDetail.jsx
+++ frontend/src/Components/ProjectDetail.jsx
@@ -115,6 +115,7 @@ const ProjectDetail = () => {
 
         messageBox.current.value.appendChild(message);
         scrollToBottom()
+        console.log()
     }
 
     const appendOutGoingMessage = (messageObject) => {
